c43861ae0014ec412d53c41865daff5a42ac82ee
Syxii <juhohei15@gmail.com>
2026-02-03 04:23:30 +0200

    Fixes

--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import fs from "fs";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./key.pem"),
-      cert: fs.readFileSync("./cert.pem"),
-    },
-    host: "localhost",
-    port: 5173,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    server: command === "serve" ? {
+      https: {
+        key: fs.readFileSync("./key.pem"),
+        cert: fs.readFileSync("./cert.pem"),
+      },
+      host: "localhost",
+      port: 5173,
+    } : undefined,  // production build ignores server config
+  };
 });
