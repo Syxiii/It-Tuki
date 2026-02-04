956cee7e9b5737e06d556b6c22f5a7795fdb5685
Syxii <juhohei15@gmail.com>
2026-02-04 21:21:45 +0200

    Removed testing keys for frontend.

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({
-    origin: "https://localhost:5173", // must match frontend exactly
+    origin: "http://localhost:5173", // must match frontend exactly
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
--- Frontend/vite-project/vite.config.js
+++ Frontend/vite-project/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from "fs";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    server: command === "serve" ? {
-      https: {
-        key: fs.readFileSync("./key.pem"),
-        cert: fs.readFileSync("./cert.pem"),
-      },
-      host: "localhost",
-      port: 5173,
-    } : undefined,  // production build ignores server config
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 5173,
+  },
 });
