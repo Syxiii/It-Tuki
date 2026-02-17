eb1e47a7bd52e5b1c857ec70f2c64ab1b5d59098
Syxiii <juhohei15@gmail.com>
2026-02-17 15:49:30 +0200

    Small fix

--- Backend/controllers/ticketController.js
+++ Backend/controllers/ticketController.js
@@ -1,20 +1,9 @@
 import prisma from "../prisma/client.js";
 import admin from "firebase-admin";
-import path from "path";
-import { fileURLToPath } from "url";
-import { readFileSync } from "fs";
+import fs from "fs";
 
-// For __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const serviceAccount = JSON.parse(fs.readFileSync("/app/firebase-key.json", "utf-8"));
 
-// Absolute path inside Docker container
-const serviceAccountPath = path.join("/app", "firebase-key.json");
-
-// Read and parse the JSON file
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf-8"));
-
-// Initialize Firebase Admin
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
